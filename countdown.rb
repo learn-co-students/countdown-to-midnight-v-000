9d3d007bdeb309b43a376bf579ca903935104536
surla <josephsurla@gmail.com>
2015-11-12 15:18:43 -0800

    updated method

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep(number)
+  while  number > 0
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+  number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
