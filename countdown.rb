1e8e4a374f3a3b0038ce89a04daeb75efaa36285
strohy1210 <sarah.stroh@flatironschool.com>
2015-12-14 11:23:01 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(n = 5)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "Happy new year!".upcase
+end
 
-def countdown
+
+def countdown_with_sleep(n = 5)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+      sleep(1.0)
+  end
+  puts "Happy new year!".upcase
+
 end
