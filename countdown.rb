e8c69d692bdb57a4fac694f6b9208e1a276024aa
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 00:46:59 -0500

    one more fix

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown(number)
-	while number >= 0
+	while number > 0
 		puts "#{number} SECOND(S)!"
 		number -=1
 	end
@@ -10,7 +10,7 @@ end
 
 
 def countdown_with_sleep(number)
-	while number >= 0
+	while number > 0
 		sleep(1)
 		puts "#{number} SECOND(S)!"
 		number -=1
