59177c451181ae07ed58f11624440faeb52fdb55
fiveinfinity <jlachance1@gmail.com>
2015-12-28 09:03:40 -0600

    added countdown and countdown with sleep methods

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+x = 10
 
-def countdown
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    #sleep(1)
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep(1)
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(x)
