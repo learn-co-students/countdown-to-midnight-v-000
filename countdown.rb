dc1b0a83680fc62fd7534a83bfdfa09088d10e9e
jjlippman <jjlippman@earthlink.net>
2016-01-31 11:31:23 -0700

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+
+  while x>0 
+    puts "#{x} SECOND(S)!"
+    x-=1
+  end
+ "Happy New Year!".upcase
+end
+
+def countdown_with_sleep(x)
+  while x>0 
+    puts "#{x} SECOND(S)!"
+    x-=1
+    sleep 1
+  end
+ "Happy New Year!".upcase
+
 end
