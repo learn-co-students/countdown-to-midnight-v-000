f0cf7e922a77e0d3bc47571cf86485b5ed58aae8
Rami97 <rami-sebastian@web.de>
2016-02-24 08:49:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number != 0 do
+    puts "#{number} SECOND(S)!".upcase
+    number -= 1
+  end
+  "happy new year!".upcase
+end
+
+def countdown_with_sleep(number)
+  while number != 0 do
+    puts "#{number} SECOND(S)!".upcase
+    number -= 1
+    sleep(1)
+  end
+  "happy new year!".upcase
 end
