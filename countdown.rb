506c18d75711078fc7e0d814223ef46eee339053
yeungn <nin.yeung@gmail.com>
2016-03-04 17:43:52 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(int)
+  sleep(int)
 end
+
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
