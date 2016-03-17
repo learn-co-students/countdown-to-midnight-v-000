78d220a5fb7c6fd603c4a26b7baa89ecf306b44c
adriandave13 <adrian@paradoxlogic.net>
2016-03-17 17:05:42 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(countdown_integer)
+
+  while countdown_integer > 0
+    puts "#{countdown_integer} SECOND(S)!"
+    countdown_integer -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(countdown_integer)
+
+  while countdown_integer > 0
+    puts "#{countdown_integer} SECOND(S)!"
+    countdown_integer -= 1
+    sleep(1)
+  end
+
+  "HAPPY NEW YEAR!"
+
 end
