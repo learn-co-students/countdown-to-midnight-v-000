c93eb88ad6b9a3fbdcd06b7da556f126125572f8
justinhamlin <justin.hamlin@gmail.com>
2016-03-22 15:33:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ while seconds > 0
 end
 end
 
-def countdown_with_sleep (seconds)
-  sleep 5
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
