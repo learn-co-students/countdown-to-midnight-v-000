529f0e1adea0af2a7b572257863779ef36f7354a
justinhamlin <justin.hamlin@gmail.com>
2016-03-22 15:31:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+def countdown (seconds)
+  seconds = 10
+while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  if seconds == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+end
 
-def countdown
+def countdown_with_sleep (seconds)
+  sleep 5
 end
