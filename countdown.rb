cc48b5e98729d445e4ae85cf165bc8e3686c4e14
mcasciato <michael.casciato@icloud.com>
2016-03-22 17:07:59 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown (number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  number == 0
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep seconds.to_i
 end
