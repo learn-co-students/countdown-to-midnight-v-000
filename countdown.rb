a5571fea5cb3948ef1ce46b8362b9ea5d94f211c
ryecox <ryecox@gmail.com>
2016-05-19 19:27:03 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+#sleep(num_secs) <<<<<function explanation below.>>>>>>> 
+#http://stackoverflow.com/questions/1329967/tell-ruby-program-to-wait-some-amount-of-time
