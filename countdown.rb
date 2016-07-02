9979e8d4730c19b969bbd2588eaf076cfd15d729
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-02 19:05:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  number = int.to_i
+  happy = "HAPPY NEW YEAR!"
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return happy
+end
+
+def countdown_with_sleep(int)
+  number = int.to_i
+  happy = "HAPPY NEW YEAR!"
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  puts happy
+  return happy
 end
