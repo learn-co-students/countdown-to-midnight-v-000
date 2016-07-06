0c352d5182fa405af3739a45edc14fc2b450e6e1
Lani1321 <lani.berry13@gmail.com>
2016-07-06 16:35:29 -0400

    use while loop to countdown to zero, and make loop pause for one second

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  numbers = seconds
+
+  while numbers > 0
+    puts "#{numbers} SECOND(S)!"
+    numbers -= 1
+  end
+"HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(seconds)
+  numbers = seconds
+
+  while numbers > 0
+    puts "#{numbers} SECOND(S)!"
+    sleep 1
+    numbers -= 1
+  end
+"HAPPY NEW YEAR!"
 end
