4f457cc05c1fbef1b1fa6c106a5d456261af3bdd
Bryna Dunnells <badunnells@gmail.com>
2016-07-09 06:39:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(currentTime)
+
+  while currentTime != 0
+    puts "#{currentTime} SECOND(S)!"
+    currentTime -= 1
+    end
+      return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(currentTime)
+
+  while currentTime != 0
+    puts "#{currentTime} SECOND(S)!"
+    sleep 1
+    currentTime -= 1
+    end
+      return "HAPPY NEW YEAR!"
 end
