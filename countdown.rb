81dde082ca7da8d67b76bc3971b4366f2e9195a2
Ryan Evans <ryanwevans@icloud.com>
2016-08-29 20:31:06 -0700

    add while loops

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  number = integer
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(integer)
+  number = integer
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+
+  "HAPPY NEW YEAR!"
 end
