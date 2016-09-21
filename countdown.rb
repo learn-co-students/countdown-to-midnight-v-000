98635f8ac9edf50ca29de36e9393cc135881a03b
Scott Wakeman <swakeman@ca.rr.com>
2016-09-21 22:32:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+#Write a method that takes in an integer argument
+def countdown(number)
+  nil.to_i
+#uses a while loop to countdown from that integer to 0
+    while number > 0
+#outputting "#{number} SECOND(S)!" in each iteration of the loop
+      puts"#{number} SECOND(S)!"
+      number-=
 
-def countdown
+      if number==0
+         puts"HAPPY HEW YEAR!"
+    end
+  end
 end
+
+def countdown_with_sleep
+  sleep(1)
+  end
