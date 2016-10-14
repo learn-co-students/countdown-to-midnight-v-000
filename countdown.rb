d33be5556425502c8626d4ec97393701b59c0b42
Mary Umoh <umohm12@gmail.com>
2016-10-14 16:55:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown (num)
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1 #num= num-1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep (n)
+sleep 5
+num = 10
+while num > 0
+  puts "#{num} SECOND(S)!"
+  num -= 1 #num= num-1
+  end
 end
