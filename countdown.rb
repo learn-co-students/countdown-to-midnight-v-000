7bd958f0338732e0959d59ae9e44f77b1944f900
David Lewitan <dovidlwtn@gmail.com>
2016-10-30 16:52:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+def countdown(seconds)
+  while seconds > 0 do
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+  while seconds >= 0 do
+    puts "#{seconds} SECOND(S)!"
+
+    t = Time.now.to_i
+    until Time.now.to_i - t >= 1 do
+    end
+
+    seconds -= 1
+  end
 end
