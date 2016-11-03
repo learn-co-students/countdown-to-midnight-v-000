7c631d8249642013ff46a5c4847e31321b5bbab9
Elias Ventura <eliasventura23@gmail.com>
2016-11-03 20:14:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,17 @@
 #write your code here
-
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+end
+"HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
     sleep(1)
-    seconds_to_midnight -= 1
+    integer -= 1
   end
   "HAPPY NEW YEAR!"
 end
