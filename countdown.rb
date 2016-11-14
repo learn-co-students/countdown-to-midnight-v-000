2c6a5d4fb68a8babc7f80bbeae2b4416794e90c9
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-14 22:56:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  until x == 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  until x == 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
