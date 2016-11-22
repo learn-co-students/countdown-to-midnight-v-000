69b045d37a15a1ab19194d993ccf81b079f5bc5b
Alexander Friedland <friedlandaj@gmail.com>
2016-11-22 02:00:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = integer
+  while counter < 0
+    counter -=1
+    puts "#{counter} SECOND(S)!"
+    if counter == 0
+      puts "HAPPY NEW YEAR!"
+    end
+  end
 end
