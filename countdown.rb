a4df50be0da8086fffab55a6b6a873aa1db791d9
Catherine Cordova <ccordova41@gmail.com>
2017-01-05 21:41:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+  #write your code here
+  def countdown(x)
+
+    x = 10
+
+  while x < 10
+  print "#{x} SECOND(S)!"
+  x -= 1
+
+end
+  until x = 0
+    print "HAPPY NEW YEAR!"
+  end
 end
