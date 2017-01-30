f50d005143e31e45a5334e6e78e4b94a39fad9eb
Isis Tejeda <isis@isistejeda.com>
2017-01-30 13:14:17 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,22 @@
 def countdown(number)
 
   while number > 0
-    puts "#{{number}} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
+
     number -= 1
   end
-  
-  puts"HAPPY NEW YEAR!"
+
+   "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+
+   "HAPPY NEW YEAR!"
 end
