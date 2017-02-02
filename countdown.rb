707e2fd7c8623fa6b709c8ab97e5e762f34d7aaf
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-02 02:38:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(i)
+  i = 10                #i = 86400 * 335 #Wayyyy too slow, and tests only for sleep anyway, not actual runtime.
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  if i == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+end
+
+def countdown_with_sleep(i)
+  i = 10
+  while i > 0
+    i -= 2
+    sleep(1)
+  end
 end
