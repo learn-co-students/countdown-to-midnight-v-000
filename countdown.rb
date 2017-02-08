af9b129972e9ba6b9b1ecf6be3e7175c1129af62
Rebecca Fisher <reb3cca.fisher@gmail.com>
2017-02-08 16:14:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -6,10 +6,10 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  while number != 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown_with_sleep(seconds)
+  while seconds != 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
     sleep 1
   end
   "HAPPY NEW YEAR!"
