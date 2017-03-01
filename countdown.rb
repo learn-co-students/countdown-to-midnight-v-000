635f152e891d6a92bbb8fe84d15cfaff81a40791
Terry Blue <terryblue@comcast.net>
2017-03-01 03:07:26 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+number = 10
 
-def countdown
+def countdown(number)
+  while number > 0
+    one_sec = 1
+    countdown_with_sleep(one_sec)
+    puts "#{number} SECOND(S)!"    
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(one_sec)
+  sleep one_sec
+end
+  
