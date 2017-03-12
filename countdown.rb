83e4904b0259bb89eff89e65669c113fde790674
irina <iafanasyevaa@gmail.com>
2017-03-12 04:50:53 +0300

    looping by using #while and #sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown (seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (seconds)
+  while seconds > 0
+    t = Time.now
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(t+1 - Time.now) #cool if the running of loop takes less them 1 (time of sleep)
+  end
+  "HAPPY NEW YEAR!"
 end
