445447d4259a563d5163ea743caa2e2c63c5580f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-03-22 00:29:16 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(n)
+  i = n
+  while i != 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  i = n
+  while i != 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
