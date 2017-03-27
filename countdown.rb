71e6ab4fcc28fbfb35208d6f65c91c1bc834c587
Victoria Sowell <tpms123@gmail.com>
2017-03-27 05:24:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(n)
+
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(runtime)
+  sleep(runtime)
+    countdown(runtime)
 end
