d8ec8e6adeeebce54ac61155390ba9b711330871
John Blank <veloblank@gmail.com>
2017-03-29 21:00:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    if int == 0
+      return "HAPPY NEW YEAR!"
+    end
+  end
+end
+
+var = 5
+def countdown_with_sleep(var)
+  while var > 0 do
+    puts "#{var} SECOND(S)!"
+    var -= 1
+    sleep 1
+    if var == 0
+      return "HAPPY NEW YEAR!"
+    end
+  end
 end
