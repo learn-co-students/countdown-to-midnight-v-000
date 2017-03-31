a6da1cd98115f0868e3732aead0fe50b274b53e6
David Mars <david.w.mars@my.occc.edu>
2017-03-31 07:35:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+#  ~~~HAVICK WAS HERE~~~
 
-def countdown
+def countdown(number)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number >=1
+  
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+    
+
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
