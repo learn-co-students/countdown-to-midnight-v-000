32950a2ec18e82fc2e95810b10631cf5e242dc30
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-13 21:46:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  x = number
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(arg)
+  x = arg
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep 1
+    x -= 1
+  end
 end
