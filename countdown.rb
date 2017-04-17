bf1f7a21ee96542248219b0c98eb005be0664e60
lucketta <lucketta@umich.edu>
2017-04-16 22:47:01 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(count)
+  while count != 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep(count)
+  while count != 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
