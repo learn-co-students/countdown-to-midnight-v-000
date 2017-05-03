81e22ff1bff464c74b89f6861a8fd406e39a008c
curlywallst <nnoyes@comcast.net>
2017-05-03 17:16:00 -0400

    first loop done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  print "Enter seconds:"
+  seconds = gets.to_i
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
