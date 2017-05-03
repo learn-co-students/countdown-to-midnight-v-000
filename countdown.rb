cddb781715e62976b141a38c9271172beea79ddb
curlywallst <nnoyes@comcast.net>
2017-05-03 17:30:39 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,23 @@
 #write your code here
 
-def countdown
-  print "Enter seconds:"
-  seconds = gets.to_i
+def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def LOOPS
+  countdown(10)
+  countdown_with_sleep(6)
 end
