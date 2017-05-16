e02aa96b8f59b19876d56df827d14bf598ac5852
Noah Summers <summers.noah@gmail.com>
2017-05-15 20:15:06 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  count = 0
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    count += 1
+  end
+  "HAPPY NEW YEAR!" if count == 12  
+end
+
+def countdown_with_sleep(n)
+  count = 0
+  while n > 0
+    sleep(1)
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    count += 1
+  end
+  "HAPPY NEW YEAR!" if count == 12  
 end
