28a06f2f7dd068fd0b6cc838d7ec1c41cdc4e90c
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-05-30 14:48:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -9,11 +9,18 @@ def countdown(number) #define a method countdown and pass it an argument number
   "HAPPY NEW YEAR!"
 end
 
-#   puts "#{number} SECOND(S)!"
-#   number -= 1
-#     while number < 10
-# end
-# # "HAPPY NEW YEAR!"
-# def countdown_with_sleep(number)
+def countdown_with_sleep(number)
+  number
+  while number >= 1  # As long as the number is greater than or equal to 1
+  puts "#{number} SECOND(S)!"  #Output the number of seconds.
+  sleep 1
+  number -= 1  #subtract one from the number and repeat until the number is 0
+  end
+  "HAPPY NEW YEAR!"
+end
 #
+# x = 1
+# while x < 10
+#   puts "#{x} is less than 10"
+#   x += 1
 # end
