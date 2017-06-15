d1fde55aefadb3aef0f876ce1515af73fdd17f9b
britt <britt.cumens@gmail.com>
2017-06-15 17:41:03 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  cheer = "HAPPY NEW YEAR!"
+  until num == 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  cheer
+end
+
+def countdown_with_sleep(num)
+  cheer = "HAPPY NEW YEAR!"
+  until num == 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  cheer
 end
