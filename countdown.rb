71655022647e56252ccd89ac82d54eeeb1926857
hansenjl <jenn.leigh.hansen@gmail.com>
2017-06-28 17:45:45 -0700

    create countdown loop

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
-
-def countdown
+def countdown(number)
+  until number == 0
+    puts "#{number} SECOND(S)!" if number < 11 && number > -11
+    number -= 1  if number > 0
+    number += 1 if number < 0
+    countdown_with_sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep(number)
+end
+
+
+
+
