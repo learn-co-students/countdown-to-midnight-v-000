e169af58d45d50bae8ba552303598b8daaaadfa2
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-22 21:54:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,22 @@
-#write your code here
-
-def countdown(number)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1;
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1;
   end
 
   return "HAPPY NEW YEAR!"
 end
 countdown(5)
 
-def countdown_with_sleep(num)
-  until num == 0
-    sleep 1
-    puts "loop #{num}. pause loop for 1 sec.."
-    num -= 1
+
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep (1)
+    seconds_to_midnight -= 1;
   end
+
+ return "HAPPY NEW YEAR!"
 end
 countdown_with_sleep(10)
