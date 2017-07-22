c1984e8b7a8b984ca90fdc36e1ebedc1c226b0d3
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-22 21:49:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1;
+  end
+
+  return "HAPPY NEW YEAR!"
 end
+countdown(5)
+
+def countdown_with_sleep(num)
+  until num == 0
+    sleep 1
+    puts "loop #{num}. pause loop for 1 sec.."
+    num -= 1
+  end
+end
+countdown_with_sleep(10)
