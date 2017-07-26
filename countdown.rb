8571b0ddc420aad255d1267674163c8df4458f01
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-07-26 12:19:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+
+def countdown(n)
+    while n > 0
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+   "HAPPY NEW YEAR!"
 end
+
+
+n = 10
+def countdown_with_sleep(n)
+    while n > 0
+      puts "#{n} SECOND(S)"
+      n -= 1
+      sleep 1
+    end
+end
+
+puts "HAPPY NEW YEAR!"
