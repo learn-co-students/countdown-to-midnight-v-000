e2e3222df335229e7b1c9f9818ac7d18827946db
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-07-30 16:49:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+n = 10
+def countdown (n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n-=1
+end
 
-def countdown
+
+"HAPPY NEW YEAR!"
+end
+
+x=5
+def countdown_with_sleep (x)
+  until x==0
+    x-=1
+    sleep(1)
+  end
 end
