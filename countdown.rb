064b0741b7e8103e70542867108a5f4ec1741d7d
Amelie Oller <amelieoller@gmail.com>
2017-08-10 20:51:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(times)
+  while times > 0
+    puts "#{times} SECOND(S)!"
+    times -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(times)
+  while times > 0
+    puts "#{times} SECOND(S)!"
+    sleep(1)
+    times -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
