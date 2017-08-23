a55fd62a3a682145254802a358afb501c835e848
Gustavo Villagrana <gusvilla303@gmail.com>
2017-08-23 22:34:23 +0000

    Correct countdown_with_sleep method

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep(number)
-  countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
