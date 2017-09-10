b796c3b51370116f903ac24d9ed16216d0a498d3
Kristen Peck <kpeck71@gmail.com>
2017-09-10 22:58:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+number = 10
 
-def countdown
-end
+def countdown(number)
+  while number < 11
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  while number == 0
+    puts "HAPPY NEW YEAR!"
+  end
+ end
