8888ad84f2ecf831bd3b788a0a3649e014956a78
mevanstron <michael@michaelgregoryevans.com>
2017-09-11 16:36:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(x)
+
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    countdown_with_sleep_(x)
+  end
+
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  sleep 5
 end
