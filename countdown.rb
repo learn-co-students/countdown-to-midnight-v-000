83bba30244c1c62b59b3c90da269aab0711b7e4a
John Tuttle <thejwt@gmail.com>
2017-09-25 23:17:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+  end
+end
+
+def countdown_with_sleep(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+  end
 end
