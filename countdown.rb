e78143dd957c6acfe36e658764b3cc19d9854067
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 20:13:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(countdown_to_midnight)
+  countdown_to_midnight = 10
+  while countdown_to_midnight > 0
+    puts "#{countdown_to_midnight} SECOND(S)!"
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_to_midnight)
+  while countdown_to_midnight > 0
+    "#{countdown_to_midnight}SECOND(S)!"
+    sleep(1)
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
