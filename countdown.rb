3455011434cfd6429481217d15f8893558aac201
Terri Riggle <codeofarmour@outlook.com>
2017-10-04 18:12:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(countdown_to_midnight)
+  while countdown_to_midnight > 0
+  puts "#{countdown_to_midnight} SECOND(S)!"
+  countdown_to_midnight -= 1
+end
+"HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(countdown_to_midnight)
+  while countdown_to_midnight > 0
+    puts "#{countdown_to_midnight} SECOND(S)!"
+    sleep(1)
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
