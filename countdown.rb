bf08585a39b95016bb9ccc7e80e38f2a22c6c1fa
Elijah Marcano <emarcano143@hotmail.com>
2017-10-11 14:42:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+#require pry
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    #binding.pry
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number1)
+  sleep(5)
 end
