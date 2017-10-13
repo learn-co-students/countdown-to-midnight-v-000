1fb2099cd51a0325ab645583c15a63e3ef914310
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-13 14:34:33 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+timer = 5
 
-def countdown
+def countdown(timer)
+  while timer > 0
+    puts "#{timer} SECOND(S)!"
+    timer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(timer)
+  while timer > 0
+    puts "#{timer} SECOND(S)!"
+    timer -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
