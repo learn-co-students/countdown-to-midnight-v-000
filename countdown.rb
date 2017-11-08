b3b576ae08e0c5783f420cdbdfeba33d4e91e0fa
Connor Gallopo <connor.gallopo@me.com>
2017-11-08 22:08:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n != 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  x = 7
+  while x != 0
+    puts "sloooow down"
+    sleep 1
+    x -= 1
+  end
 end
