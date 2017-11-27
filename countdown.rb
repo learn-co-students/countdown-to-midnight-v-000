b60be815f606beefcbfb1ab8f8dd772a97ef69ce
Annette <micha232@umn.edu>
2017-11-27 07:33:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(i=1)
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+# Countdown method with a delay between loops
+def countdown_with_sleep(i)
+    while i >= 0
+        puts "#{i} SECOND(S)!"
+        sleep(1)
+        i -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
