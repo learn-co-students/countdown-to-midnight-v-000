950a8beb353c621159a762c281064ea1a157abc1
Man Leung <lepusleung@gmail.com>
2017-12-07 20:10:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(number)
+  number = number +1
+  until number == 1
+    number -=1
+    puts "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  number = number +1
+  until number == 1
+    number -=1
+    puts "#{number} SECOND(S)!"
+    sleep 5
+  end
+  return "HAPPY NEW YEAR!"
 end
