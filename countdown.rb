10da609cf09074913d124810986604f7410ccb16
rh24 <ms.rebeccahong@gmail.com>
2017-12-12 13:47:17 -0500

    added countdown to countdown_with_sleep

--- countdown.rb
+++ countdown.rb
@@ -4,15 +4,15 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    if number == 0
-      return "HAPPY NEW YEAR!"
-    end
   end
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
   until number == 0
+    puts "#{number} SECOND(S)!"
     sleep(1)
     number -= 1
   end
+  "HAPPY NEW YEAR!"
 end
