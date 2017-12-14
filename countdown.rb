14eb76114dd9096be2a17465edaf7d4c9a182c06
kim alban <kimalbans@yahoo.com>
2017-12-14 03:02:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
-
+counter = 10
 def countdown
+  loop do
+  counter 1= 0
+  if counter = 0
+  puts "HAPPY NEW YEAR!"
+end  
 end
+
+def countdown_with_sleep(5)
+end
+
+countdown_with_sleep(5)
+countdown
