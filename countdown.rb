8b4acb4dbd4bd08fffa8ecb7abfe005786a4e041
Ryan <neodynia@icloud.com>
2017-12-20 11:55:14 -0500

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown (i)
+    i = 10 
+    until i == 0
+        puts "#{i} SECOND(S)!"
+    i-= 1
+    end 
+    return "HAPPY NEW YEAR!"
+end 
 
-def countdown
-end
+def countdown_with_sleep(i)
+    sleep(i)
+end 
