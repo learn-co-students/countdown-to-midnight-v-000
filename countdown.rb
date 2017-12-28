1dc6b384430c73306b5feb90ec31c1cfcd70062e
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-28 13:48:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(second)
+
+    if second == 12
+      "HAPPY NEW YEAR!"
+    else
+      while second > 0
+        puts "#{second} SECOND(S)!"
+        second -= 1
+      end
+    end
+
+end
+
+def countdown_with_sleep(second)
+  sleep(second)
+  "HAPPY NEW YEAR!"
 end
