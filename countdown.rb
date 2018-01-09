844cf02e9e9b899b58189ef7862f6ce9059bc38d
jacobsilver2 <jacobsilver2@mac.com>
2018-01-09 10:25:55 -0500

    did the work

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
-
-def countdown
+def countdown(int)
+   while int >= 0
+      puts "#{int} SECOND(S)!"
+      int -= 1
+   end
+   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+   while x >= 0
+      puts "#{x} SECOND(S)!"
+      x -= 1
+      sleep 1
+   end
+   puts "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(10)
