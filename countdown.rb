ee1c3abfbf2dc6b51f7571b9056196b5bf9b31f9
Trac Nguyen <nguyendtrac@gmail.com>
2018-01-22 04:26:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(counter)
+  c = counter
+  while counter > 0
+    puts "#{c} SECOND(S)!"
+    c -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(counter)
+  c = counter
+  while c > 0
+    puts "#{c} SECOND(S)!"
+    sleep(1)
+    c -=1
+  end
+  "HAPPY NEW YEAR!"
 end
