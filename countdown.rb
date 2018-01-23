a6c90f073a9f583b061678aac4aad8e0ab8f4233
Harley Rayner <harleymteden@gmail.com>
2018-01-23 02:02:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(time_on_clock)
+
+  while time_on_clock != 0
+    puts "#{time_on_clock} SECOND(S)!"
+    # time_on_clock
+    time_on_clock -= 1
+  end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_on_clock)
+
+ while time_on_clock != 0
+    puts "#{time_on_clock} SECOND(S)!"
+    sleep 2
+    time_on_clock -= 1
+  end
+    "HAPPY NEW YEAR!"
 end
