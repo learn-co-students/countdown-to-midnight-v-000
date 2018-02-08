63c7ae8d35ffe070bb04ab5d02850f5f4a8f6001
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 21:05:26 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    while x > 0
+      puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  while n > 0
+  sleep 1
+  n -= 1
+  end
+end
+
+
+
+# def countdown(x)
+#     while x > 0
+#       puts "#{x} SECOND(S)!"
+#       sleep 1
+#         x -= 1
+#     end
+#   return "HAPPY NEW YEAR!"
+# end
