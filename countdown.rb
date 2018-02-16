e74dcf16bf18d75c1438ad7dc8b021d41fe0b8af
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-02-16 22:48:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,3 @@
-#write your code here
-def countdown(number)
-  until number == 0
-    puts "#{number} SECOND(S)!" if number < 11 && number > -11
-    number -= 1  if number > 0
-    number += 1 if number < 0
-    countdown_with_sleep(1)
-  end
-  "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(number)
-  sleep(number)
-end
-
 
 
 
