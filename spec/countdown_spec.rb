974f91201e6ea7505e39804d75b9d43e4a1c6a36
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-02-17 02:31:08 +0000

    Done.

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -3,7 +3,7 @@ require 'benchmark'
 
 describe 'LOOPS' do
 
-  describe '#countdown' do
+  describe '#coucount = ntdown' do
     let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
 
     it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
