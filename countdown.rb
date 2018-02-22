68569eb27d3cb88e3afb315e4c95e643f002a5b1
David McGrann <dpmcgrann@gmail.com>
2018-02-22 01:48:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+STDOUT.sync = true
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  sleep 5
 end
