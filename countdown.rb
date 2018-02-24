594d808dedeb75a2533970dfd1549e5e520c1d03
Kristin Reddington <kristinreddington@gmail.com>
2018-02-24 23:44:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(10)
+
+while number < 10
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep()
+  while 
+  sleep 1 
 end
