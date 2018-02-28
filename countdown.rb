0b18d1952b8576ff3ec5e2c1dd68a16ca9bb5c9e
Aspen James <aspenjames@tqca.org>
2018-02-28 22:43:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(s)
+  while s > 0
+    puts "#{s} SECOND(S)!"
+    s -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(s)
+  while s > 0
+    puts "#{s} SECOND(S)!"
+    s -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
