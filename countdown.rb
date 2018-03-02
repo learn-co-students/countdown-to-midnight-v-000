d730073c9104ed0d37d06ec66036b1ca7020475b
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 18:44:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,20 @@
 def countdown(x)
   x = 10
   while x > 0
-    puts "#{x} SECOND(S)!"
+    puts "#{x} SECOND(S)!" (sleep 2)
     x -= 1
 end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(sleep)
-  
-end
+countdown(10)
+#def countdown_with_sleep(integer)
+#  integer = 3
+#  while x > 0
+#    x -=1
+#   sleep 5
+#end
+#end
 #def using_until
         #levitation_force = 6
       #until levitation_force == 10
