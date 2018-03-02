ee1ea1b7801a9502a6bf5e0496d5366e4c93d281
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 00:08:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+def countdown(x)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sleep)
+  
+end
+#def using_until
+        #levitation_force = 6
+      #until levitation_force == 10
+        #puts "Wingardium Leviosa"
+        #levitation_force += 1
+      #end
+#end
