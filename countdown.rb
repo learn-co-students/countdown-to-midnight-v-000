9b6f988360d9a950b62e13e7a2a0b78a0c9b328f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-15 16:48:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+    #flipped puts and number order because started counting 1 number too early
+    #originally was
+    #number = number - 1
+    #puts "#{number} SECOND(S)!"
+  end
+
+  return 'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number = number - 1
+  end
+
+  return 'HAPPY NEW YEAR!'
 end
