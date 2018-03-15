bd662a9d3e0c8923bd7d6e6fa997db752305d1bd
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-15 15:57:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(sec)
+  sec = sec
+  while sec > 0
+    puts "#{sec} SECOND(S)!"
+  sec -=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+  sleep(num_secs)
 end
