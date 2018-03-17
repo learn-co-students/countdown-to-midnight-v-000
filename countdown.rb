437abb4cbf1c401c669564de97ef5ce98fe55da8
David Chiu <davidwchiu@gmail.com>
2018-03-17 20:10:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,19 @@
 #write your code here
+require 'pry'
 
-def countdown(integer)
-  while integer > 0
-    puts "#{{number} SECOND(S)!"
-    integer -= 1
-  end
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+end
+"HAPPY NEW YEAR!"
 end
