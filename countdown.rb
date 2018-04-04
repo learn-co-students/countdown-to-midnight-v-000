ec714b306dd8d38ce3c3efbadb11fc56856a8489
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-04 22:09:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
 
-def countdown
+def countdown(counter)
+  until counter == 0 do
+    puts "#{counter} SECOND(S)!"
+    counter = counter - 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown)
+   sleep 5
 end
