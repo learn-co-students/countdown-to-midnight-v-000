a11f7a02b3b4e9c9a888cd74ccaef34b15128069
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-17 03:46:59 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  greeting = "HAPPY NEW YEAR!"
+  count = seconds
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  greeting
 end
+
+def countdown_with_sleep(units)
+  sleep 5
+end
+
+#start_time = Time.now
+#end_time = start_time + 25
+#puts start_time
+#puts end_time
