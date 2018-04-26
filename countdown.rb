ed90382c69184040e3f36a937ee8f2e1d2c57c7f
Halyette <haly.lesane@gmail.com>
2018-04-26 12:47:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+ n = 5
+
+def countdown(x)
+  
+while n > 0 
+  puts "#{n} SECONDS(S)!"
+  n -= 1 
+  if n == 0
+  
+ "HAPPY NEW YEAR!"
+
+end
+end
+
+def countdown_with_sleep(5)
+  countdown(5)
+    sleep 1
 end
