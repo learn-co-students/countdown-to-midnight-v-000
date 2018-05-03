865afd9a542c81513d063ddebe872e1a003409ee
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-03 18:43:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter =10
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    counter -= 1
+end
+puts "HAPPY NEW YEAR!"
+end
 end
