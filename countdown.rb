ed220746b81a00c71a114f9064bc63150d85a76b
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-21 19:37:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,15 @@
 def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
-    seconds -=
+    seconds -= 1
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
 end
