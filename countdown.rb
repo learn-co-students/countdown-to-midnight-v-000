56bac8ddc5e5b69178c50066e61171fb3466fed1
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-27 22:06:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+require 'pry'#write your code here
 
-def countdown
+# number = 10
+# while number < 10
+#   puts "#{number} SECOND(S)!"
+#   number -= 1
+#   # return "HAPPY NEW YEAR!"
+# end
+def countdown_with_sleep(integer)
+  sleep(integer)
+end
+
+
+def countdown(x)
+    x = 10
+    while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(0.5)
+    end
+  return "HAPPY NEW YEAR!"
 end
