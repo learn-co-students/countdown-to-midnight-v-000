d90ea9e03f7e91ff46d3c3774d8b20b5f78f8e28
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-31 05:02:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+#write your code her
+def countdown(number)
+  counter = number 
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  counter = number
+  while number > 0 
+   puts "#{number} SECOND(S)!"
+  number -= 1 
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
