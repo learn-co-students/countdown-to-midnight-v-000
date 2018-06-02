e9dd78c8e7f86a2d0f93097cfbe9d8c6c6b42ce5
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 19:17:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 ##
-#
+# #countdown method counts from the input until 0 then returns "HAPPY NEW YEAR"
 ##
 def countdown(number)
   while (number > 0) do
@@ -9,6 +9,9 @@ def countdown(number)
   "HAPPY NEW YEAR!";
 end
 
+##
+# #countdown_with_sleep counts down from the input until 0 with one second between each number then returns "HAPPY NEW YEAR!"
+##
 def countdown_with_sleep(number)
   while (number > 0) do
     puts "#{number} SECONDS!";
