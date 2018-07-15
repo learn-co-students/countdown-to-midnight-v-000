5a0bb6ee753bd924626ea323ed3b1401b097490d
Joann Pan <joannlpan@gmail.com>
2018-07-15 03:34:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,16 @@
-def countdown(seconds_until_midnight)
-   while seconds_until_midnight > 0
-     seconds_until_midnight-=0
-    if seconds_until_midnight==0
-       return "HAPPY NEW YEAR!"
-    else
-        puts "-#{ seconds_until_midnight} SECOND(S)!"
-    end
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
