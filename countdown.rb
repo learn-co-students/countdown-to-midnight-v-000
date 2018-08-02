08a7cd73df499211f9fbc5423bdd3327ee55ced2
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-02 04:37:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  while number > 0 do
+    puts "#{number] SECONDS!)"
+    sleep 1
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
