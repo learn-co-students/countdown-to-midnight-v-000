78aee36da6c9ed4609eef07a505829a146d81835
Brad McCollum <btmcco8@gmail.com>
2018-08-10 14:49:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(n) #method to countdown from value 'n' and returns a messsage when completed
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n) #method to realistically countdown from value 'n' and return a message when completed
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+ return "HAPPY NEW YEAR!"
 end
