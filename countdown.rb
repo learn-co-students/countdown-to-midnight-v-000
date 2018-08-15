37a13a4c9b84d9f139452ab3c38d241030172b67
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 12:38:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(i)
+  while i>0 
+    puts "#{i} SECOND(S)!"
+    i -= 1 
+  end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+ while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep 1 
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end 
+
+  #countdown
+    # outputs '<n> SECOND(S)!' string to STDOUT for each count (FAILED - 1)
+    # returns HAPPY NEW YEAR! (FAILED - 2)
+  #countdown_with_sleep
+    # should take at least 5 seconds to execute (FAILED - 3)
