6e546b9ecbee57868b8b3959aedd1cd6c450d576
Heather Miller <heather.miller114@gmail.com>
2018-08-24 01:05:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(timer)
+  until timer == 0
+    puts "#{timer} SECOND(S)!"
+    timer -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(timer)
+  while timer > 0
+    puts "#{timer} SECOND(S)!"
+    sleep(1)
+    timer -= 1
+  end
 end
