65d1154b0b1783b7777e7d50e79daa884652f760
Ashley Wong <ashwong04@gmail.com>
2018-08-28 13:41:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,12 @@ def countdown(secs)
   while secs > 0
     puts "#{secs} SECOND(S)!"  
     secs -= 1
-    #countdown_with_sleep
+    countdown_with_sleep
   end
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep
+  sleep(1.5)
+  
+end
