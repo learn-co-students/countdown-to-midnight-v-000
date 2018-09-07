f6251c7cc4371c9b6b31a1b0b620193173fb6c46
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-07 01:05:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    numberString = number.to_i
+    puts "#{numberString} SECOND(S)!"
+    number = number - 1
+  end
+  returnvalue = "HAPPY NEW YEAR!"
+  returnvalue
+end
+
+
+def countdown_with_sleep(number)
+  until number == 0 
+    puts "#{number} SECOND(S)"
+    number = number - 1
+    sleep 1
+  end
+  returnvalue = "HAPPY NEW YEAR!"
+  returnvalue
 end
