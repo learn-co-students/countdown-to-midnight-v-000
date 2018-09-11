7c492d3454f9fb6e2e8332820a80f115bf60edba
veloblank <veloblank@gmail.com>
2018-09-11 16:22:52 -0400

    Refactor

--- countdown.rb
+++ countdown.rb
@@ -1,23 +1,17 @@
-#write your code here
-
-def countdown(int)
-  while int > 0 do
-    puts "#{int} SECOND(S)!"
-    int -= 1
-    if int == 0
-      return "HAPPY NEW YEAR!"
-    end
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
   end
+  "HAPPY NEW YEAR!"
 end
 
-var = 5
-def countdown_with_sleep(var)
-  while var > 0 do
-    puts "#{var} SECOND(S)!"
-    var -= 1
-    sleep 1
-    if var == 0
-      return "HAPPY NEW YEAR!"
-    end
+
+def countdown_with_sleep(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(1)
   end
+  "HAPPY NEW YEAR!"
 end
