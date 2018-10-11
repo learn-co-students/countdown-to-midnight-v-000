d24bb51828e897422dea05602f0c06ee51b89822
Vince Andrew Pimentel <contact@vincep.me>
2018-10-11 23:58:54 +0000

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+i = 5
 
-def countdown
+def countdown(i)
+  while i != 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(i)
+  sleep i
 end
