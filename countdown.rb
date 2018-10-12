97cf9706e8e09b3da43ad48dba7c49ee33d0bbbe
Vince Andrew Pimentel <contact@vincep.me>
2018-10-12 00:01:55 +0000

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,4 @@
 #write your code here
-i = 5
-
 def countdown(i)
   while i != 0
     puts "#{i} SECOND(S)!"
@@ -10,5 +8,9 @@ def countdown(i)
 end
 
 def countdown_with_sleep(i)
-  sleep i
+  while i != 0
+    puts "#{i} SECONDS(S)!"
+    i -= 1
+    sleep 1
+  end
 end
