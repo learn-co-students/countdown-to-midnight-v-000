cb20f9c8c96c72c1004e775936a2c9ba92e48a3d
ktochow1 <ktochow1@binghamton.edu>
2018-10-16 17:49:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(timer)
+  timer
+  while timer > 0  
+  puts "#{timer} SECOND(S)!"
+  timer -= 1
 end
+  if timer == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(sec)
+  timer = 5
+  while timer > 0 
+  sleep(1)
+  puts "#{timer} SECOND(S)!"
+  timer -= 1
+end
+end
+
