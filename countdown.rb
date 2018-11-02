fc8699d0389cc68e9ba112190d43d64e2d9b11ae
Kimberly Aviles <01010110.v@gmail.com>
2018-11-02 02:05:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x.times do
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+    x.times do
+      puts "#{x} SECOND(S)!"
+      sleep(1) #tells program to wait an arbitrary amount of time
+      x -= 1
+    end
+  return "HAPPY NEW YEAR!"
 end
