3ab543270948293a567a3ca498d919a2c1aff26f
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-01 05:25:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,17 @@
 #write your code here
-number = gets.chomp.to_i
 def countdown(number)
   while number>0
     puts "#{number} SECOND(S)!"
   number -=1
-  puts "HAPPY NEW YEAR!"
 end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number>0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -=1
+end
+ "HAPPY NEW YEAR!"
 end
