407b31c1b3a2c091a84718778247056c846d4836
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-01 04:57:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
-def countdown
+number = gets.chomp.to_i
+def countdown(number)
+  while number>0
+    puts "#{number} SECOND(S)!"
+  number -=1
+  puts "HAPPY NEW YEAR!"
+end
 end
